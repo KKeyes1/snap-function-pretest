451182ffbf0407dc313ea3c37c603645ba9cb6c0
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-17 11:23:28 -0700

    string repeat working :)

--- string-repeat.php
+++ string-repeat.php
@@ -7,29 +7,30 @@
 	<body>
 	<?php
 	session_start();
-	$times = $_POST["string-repeat"];
-	$strlen = strlen($_POST["string"]);
 
-	function stringRepeat ($times, $strlen) {
-		$newString = "";
-		for ($i = 0; $i <= $strlen; $i=$i+1) {
-			$char = substr($str, $i, 1);
-			$newString = $newString + $char;
+		$input = trim($_POST["times"]);
+		$str = trim($_POST["string"]);
+
+		function stringRepeat($input, $str) {
+			$newString = "";
+			for($k = 1; $k <= $input; $k = $k + 1) {
+				for($i = 0; $i < strlen($str); $i = $i + 1) {
+					$char = $str{$i};
+					echo $char;
+				}
+
+			}
+
 		}
-		echo $newString;
-	}
 
-
-
-
-
+	stringRepeat ($input, $str);
 
 	?>
 		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" id="string-repeat" method="post">
 			<div>
 				<?php echo "Enter your string and the number of times you want to repeat it!"; ?>
 				<input type="text" id="string" name="string">
-				<input type="text" id="times" name="times">
+				<input type="number" id="times" name="times">
 				<input type="submit" name="string-repeat">
 			</div>
 		</form>
